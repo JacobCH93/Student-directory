10f58c87c6037f9c37959abb73a99cb9b72e53ea
EC2 Default User <ec2-user@ip-172-31-6-252.eu-west-2.compute.internal>
2021-10-12 08:50:03 +0000

    directory.rb

--- directory.rb
+++ directory.rb
@@ -1,18 +1,24 @@
-students = ["Dr. Evil", "MiniMe", "Scott Evil",
-"Nurse Ratchet", "Octopussy", "Joker"]
+students = [
+{name: "Dr. Evil", cohort: :october},
+{name: "MiniMe", cohort: :october},
+{name: "Scott Evil", cohort: :october},
+{name: "Nurse Ratchet", cohort: :october},
+{name: "Octopussy", cohort: :october},
+{name: "Joker", cohort: :october}
+]
 def print_header
 puts "The students of Villians Acadamy"
 puts "--------------------------------"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "We have #{names.count} very evil students!"
+def print_footer(students)
+  puts "We have #{students.count} very evil students!"
 end
 print_header
 print(students)
